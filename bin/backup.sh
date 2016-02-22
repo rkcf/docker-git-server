06d46d1fd49d862eb3c302a92aacaea4df258e9c
EC2 Default User <ec2-user@ip-172-31-36-195.us-west-2.compute.internal>
2016-02-22 15:42:01 +0000

    fixed branch issues

--- bin/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-DATE=`date +%d-%m-%y`
-NAME=$DATE"_backup.tar.gz"
-HOST_LOCATION="/var/container_data/git/backups"
-docker run --rm  --volumes-from git_ssh_server -v $HOST_LOCATION:/backup \
-	ubuntu:14.04 tar cvzf /backup/$NAME /git
